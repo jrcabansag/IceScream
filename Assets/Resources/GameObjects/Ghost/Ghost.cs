bb0e458a0c218a795cd6da9362611f7e79d5e22a
JR <cabansag@stanford.edu>
2018-03-17 16:28:57 -0700

    Added stage boundaries

--- Assets/Resources/GameObjects/Ghost/Ghost.cs
+++ Assets/Resources/GameObjects/Ghost/Ghost.cs
@@ -361,4 +361,12 @@ public class Ghost : MonoBehaviour {
 			healthBar.Hide ();
 		}
 	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.transform.tag == "Player") {
+			Vector3 myForward = transform.forward;
+			myForward.y = 0f;
+			col.transform.GetComponent<PlayerScript> ().HitEnemy (Vector3.Normalize(myForward), 500f);
+		}
+	}
 }
